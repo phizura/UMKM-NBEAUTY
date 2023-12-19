3da365eacd1cad263c8a7fdbdf160176863498be
Jakii971 <izzudinlagi971@gmail.com>
2023-12-19 09:50:13 +0700

    selasa hore

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var navbar = document.getElementById("navbar");
+
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  }
+});
