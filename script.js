ef30ba42c93d9e3ce246ea7445f4fc77ee7a778d
Jakii971 <izzudinlagi971@gmail.com>
2023-12-27 13:15:22 +0700

    about us tengah, hover choose us

--- script.js
+++ script.js
@@ -25,3 +25,20 @@ $(document).ready(function () {
   });
 });
 
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 15,
+  nav: true,
+  responsive: {
+      0: {
+          items: 1
+      },
+      600: {
+          items: 2
+      },
+      1000: {
+          items: 3
+      }
+  }
+})
+
