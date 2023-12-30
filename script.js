b3a354a7b21bed91e65c04d0302397d028f7c6a5
Rafly_Rizqullah <104709795+RizqullahY@users.noreply.github.com>
2023-12-30 13:40:32 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -24,3 +24,21 @@ $(document).ready(function () {
     $(".navbar-collapse").collapse("hide");
   });
 });
+
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 15,
+  nav: true,
+  responsive: {
+      0: {
+          items: 1
+      },
+      600: {
+          items: 2
+      },
+      1000: {
+          items: 3
+      }
+  }
+})
+
