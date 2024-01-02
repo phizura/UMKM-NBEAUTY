14c3ff538d77c2c9eaf08df745a7e1765b714b0f
Jakii971 <izzudinlagi971@gmail.com>
2024-01-02 20:49:57 +0700

    hah

--- script.js
+++ script.js
@@ -25,20 +25,25 @@ $(document).ready(function () {
   });
 });
 
-$('.owl-carousel').owlCarousel({
+$(".owl-carousel").owlCarousel({
   loop: true,
   margin: 15,
   nav: true,
   responsive: {
-      0: {
-          items: 1
-      },
-      600: {
-          items: 2
-      },
-      1000: {
-          items: 3
-      }
-  }
-})
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
 
+$(document).ready(function () {
+  $(".navbar-toggler").click(function () {
+    $(".navbar-nav").toggleClass("text-center");
+  });
+});
